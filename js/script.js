c6cc715e154824ec139b7468ed74a2402984ea6b
Kevin-Vazquez-Vega <kvazquezvega@hawk.iit.edu>
2023-10-31 20:11:45 -0500

    added js validation

--- js/script.js
+++ js/script.js
@@ -4,5 +4,16 @@ jQuery(function($){
 });
 
 function formConfirmation() {
+
+    let x = document.getElementById("email").value;
+
+    if (x.includes ("@")) {
+        alert("Valid Email")
+    }
+
+    else {
+        alert("Invalid Email")
+    }
+
     alert("Thank you for your time. A link should be sent to your E-Mail for you to download Hollow Knight: Silksong. \n\nEnjoy!");
 }
